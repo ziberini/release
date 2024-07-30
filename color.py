b5882c7d01c2a748a933f8b1a1ad82e30f596890
momer <momer@momer.com>
2024-07-30 09:55:46 -0400

    testing

--- color.py
+++ color.py
@@ -1,26 +1,25 @@
-import yaml
-from github import Github
 import os
-# import warnings
+import yaml
+from github import Github, Auth
 from colorama import init, Fore, Style
-
-# # Suppress specific warnings globally
-# from urllib3.exceptions import NotOpenSSLWarning
-# warnings.filterwarnings('ignore', category=NotOpenSSLWarning)
 
 # Initialize colorama
 init()
 
 # Load the YAML configuration file
+
+
 def load_config(file_path):
     with open(file_path, 'r') as file:
         return yaml.safe_load(file)
 
 # Create a git tag on GitHub
+
+
 def create_git_tag(repo, tag):
     try:
-        # Get the latest commit SHA
-        commits = repo.get_commits()
+        # Get the latest commit SHA from the main branch
+        commits = repo.get_commits(sha='main')
         latest_commit_sha = commits[0].sha
 
         # Create the tag object
@@ -37,11 +36,15 @@ def create_git_tag(repo, tag):
             sha=tag_object.sha
         )
 
-        print(Fore.GREEN + f"Tag {tag} created for {repo.name}" + Style.RESET_ALL)
+        print(
+            Fore.GREEN + f"Tag {tag} created for {repo.name} on branch main" + Style.RESET_ALL)
     except Exception as e:
-        print(Fore.RED + f"Failed to create tag for {repo.name}: {e}" + Style.RESET_ALL)
+        print(
+            Fore.RED + f"Failed to create tag for {repo.name} on branch main: {e}" + Style.RESET_ALL)
 
 # Create a release on GitHub
+
+
 def create_release(repo, tag, release_name, release_notes):
     try:
         release_message = "\n".join(f"- {note}" for note in release_notes)
@@ -52,11 +55,15 @@ def create_release(repo, tag, release_name, release_notes):
             draft=False,
             prerelease=False
         )
-        print(Fore.GREEN + f"Release {release_name} created for {repo.name} with tag {tag}" + Style.RESET_ALL)
+        print(
+            Fore.GREEN + f"Release {release_name} created for {repo.name} with tag {tag}" + Style.RESET_ALL)
     except Exception as e:
-        print(Fore.RED + f"Failed to create release for {repo.name}: {e}" + Style.RESET_ALL)
+        print(
+            Fore.RED + f"Failed to create release for {repo.name}: {e}" + Style.RESET_ALL)
 
 # Main function
+
+
 def main():
     config = load_config('repos.yaml')
 
@@ -64,8 +71,10 @@ def main():
     token = os.getenv('GITHUB_TOKEN')
     if not token:
         raise ValueError("GITHUB_TOKEN environment variable is not set")
-    
-    g = Github(token)
+
+    # using an access token
+    auth = Auth.Token(token)
+    g = Github(auth=auth)
 
     for repo_info in config['repositories']:
         if repo_info['enabled']:
@@ -77,19 +86,23 @@ def main():
                 # Check if release with the specified tag already exists
                 releases = repo.get_releases()
                 if any(release.tag_name == tag for release in releases):
-                    print(Fore.YELLOW + f"Release with tag {tag} already exists for {repo_name}. Skipping..." + Style.RESET_ALL)
+                    print(
+                        Fore.YELLOW + f"Release with tag {tag} already exists for {repo_name}. Skipping..." + Style.RESET_ALL)
                     continue
 
-                # Create the git tag
+                # Create the git tag from the main branch
                 create_git_tag(repo, tag)
-                
+
                 # Create the release with the specified tag
                 create_release(repo, tag, tag, release_notes)
             except Exception as e:
                 if "404" in str(e):
-                    print(Fore.RED + f"Repository {repo_name} not found. Please check the repository name and your permissions." + Style.RESET_ALL)
+                    print(
+                        Fore.RED + f"Repository {repo_name} not found. Please check the repository name and your permissions." + Style.RESET_ALL)
                 else:
-                    print(Fore.RED + f"Error accessing repository {repo_name}: {e}" + Style.RESET_ALL)
+                    print(
+                        Fore.RED + f"Error accessing repository {repo_name}: {e}" + Style.RESET_ALL)
+
 
 if __name__ == "__main__":
     main()
